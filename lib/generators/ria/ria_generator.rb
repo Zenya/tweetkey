6854b0b368df2343544aeeb38141e4ce2a07ab67
barthezslavik <barthez.slavik@gmail.com>
2011-12-25 03:53:15 +0200

    generator in progress

--- lib/generators/ria/ria_generator.rb
+++ lib/generators/ria/ria_generator.rb
@@ -1,46 +1,47 @@
+class String
+  def initial
+    self[0,1]
+  end
+end
+
 class RiaGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-  argument :layout_name, :type => :string, :default => "proposal"
-  class_option :stylesheet, :type => :boolean, :default => true, :description => "Include stylesheet file"
+  argument :name, :type => :string, :default => "proposal"
+  #class_option :stylesheet, :type => :boolean, :default => true, :description => "Include stylesheet file"
   
   def generate_layout
-    copy_file "stylesheet.css", "app/assets/stylesheets/#{file_name}.sass" if options.stylesheet?
-    template "layout.html.erb", "app/#{file_name}/views/index.html.haml"
+    template "_model.html.haml", "app/views/#{plural_name}/_#{file_name}.html.haml"
+    template "index.js.haml", "app/views/#{plural_name}/index.js.haml"
+    template "controller.rb", "app/controllers/#{plural_name}_controller.rb"
+    #copy_file "index.html.haml", "app/controllers/#{file_name}/index.html.haml"
+    #copy_file "index.html.haml", "app/controllers/#{file_name}/index.html.haml"
+    #copy_file "index.html.haml", "app/controllers/#{file_name}/index.html.haml"
+    #copy_file "index.html.haml", "app/controllers/#{file_name}/index.html.haml"
+    #copy_file "index.html.haml", "app/controllers/#{file_name}/index.html.haml"
+    #copy_file "index.html.haml", "app/controllers/#{file_name}/index.html.haml"
+    #copy_file "index.html.haml", "app/controllers/#{file_name}/index.html.haml"
+    #template "layout.html.erb", "app/#{file_name}/views/index.html.haml"
   end
   
   private
   def file_name
-    layout_name.underscore
+    name.underscore
+  end
+
+  def plural_name
+    name.pluralize
   end
   
- #     invoke  mongoid
- #     create    app/models/proposal.rb
-  #    invoke    test_unit
-  #    create      test/unit/proposal_test.rb
-  #    create      test/fixtures/proposals.yml
-   #    route  resources :proposals
-    #  invoke  scaffold_controller
-     # create    app/controllers/proposals_controller.rb
- #     invoke    erb
-  #    create      app/views/proposals
-   #   create      app/views/proposals/index.html.erb
-    #  create      app/views/proposals/edit.html.erb
-     # create      app/views/proposals/show.html.erb
- #     create      app/views/proposals/new.html.erb
-  #    create      app/views/proposals/_form.html.erb
-   #   invoke    test_unit
-    #  create      test/functional/proposals_controller_test.rb
-    #  invoke    helper
-     # create      app/helpers/proposals_helper.rb
-  #    invoke      test_unit
- #     create        test/unit/helpers/proposals_helper_test.rb
-   #   invoke  assets
-  #    invoke    js
-   #   create      app/assets/javascripts/proposals.js
-   #   invoke    scss
-   #   create      app/assets/stylesheets/proposals.css.scss
-   #   invoke  scss
-  # identical    app/assets/stylesheets/scaffolds.css.scss
+#   create app/models/proposal.rb
+#   route  resources :proposals
+#   create app/controllers/proposals_controller.rb
+#   create app/views/proposals
+#   create app/views/proposals/index.html.erb
+#   create app/views/proposals/edit.html.erb
+#   create app/views/proposals/show.html.erb
+#   create app/views/proposals/new.html.erb
+#   create app/views/proposals/_form.html.erb
+#   create app/assets/javascripts/proposals.js
 
 end
